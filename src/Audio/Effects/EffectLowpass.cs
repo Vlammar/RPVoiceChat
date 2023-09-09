e36e16ee8e7bcb37cd84d808b3e01f4f4d7ef120
Rasmus Tanggaard <rasmus@tthost.dk>
2023-09-09 13:59:33 +0200

    Changed reverb to function off of base effect and added a lowpass effect as well

---
+++ src/Audio/Effects/EffectLowpass.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK.Audio.OpenAL;
+
+namespace RPVoiceChat.Audio
+{
+    public class EffectLowpass : EffectBase
+    {
+        public EffectLowpass(EffectsExtension efx, int source)  : base(efx, source ,EfxEffectType.Equalizer)
+        {
+            SetEffectProperty(EfxEffectf.EqualizerHighCutoff, 0f);
+            SetEffectProperty(EfxEffectf.EqualizerHighGain, 0f);
+        }
+
+        public void SetGain(float gain)
+        {
+            SetEffectProperty(EfxEffectf.EqualizerHighGain, gain);
+        }
+
+        public void SetCutoff(float cutoff)
+        {
+            SetEffectProperty(EfxEffectf.EqualizerHighCutoff, cutoff);
+        }
+    }
+}
--- src/Audio/Effects/EffectReverb.cs
+++ src/Audio/Effects/EffectReverb.cs
@@ -1,31 +1,84 @@
 ﻿using OpenTK.Audio.OpenAL;
+using System.Collections.Generic;
 
 namespace RPVoiceChat.Audio
 {
-    public class ReverbEffect
+    public class ReverbEffect : EffectBase
     {
-        private int effect;
-        private int slot;
-        private EffectsExtension efx;
-        public ReverbEffect(EffectsExtension efx, int source)
+        // Literally just used copilot for these values. I have no idea what they mean.
+        private Dictionary<string, float[]> reverbPresets = new Dictionary<string, float[]>
         {
+            { "Default", new float[] { 3.0f, 0.91f, 0.7f, 0.9f, 3.1f, 0.723f, 0.03f, 0.23f } },
+            { "Cave", new float[] { 2.91f, 1.3f, 1.0f, 0.5f, 2.91f, 0.5f, 0.1f, 0.23f } },
+            { "Something else", new float[] { 3.0f, 0.91f, 0.7f, 0.9f, 3.1f, 0.723f, 0.03f, 0.23f } }
+        };
 
-            this.efx = efx;
-            effect = efx.GenEffect();
-            slot = efx.GenAuxiliaryEffectSlot();
 
-            efx.BindEffect(effect, EfxEffectType.Reverb);
-            efx.Effect(effect, EfxEffectf.ReverbDecayTime, 3.0f);
-            efx.Effect(effect, EfxEffectf.ReverbDecayHFRatio, 0.91f);
-            efx.Effect(effect, EfxEffectf.ReverbDensity, 0.7f);
-            efx.Effect(effect, EfxEffectf.ReverbDiffusion, 0.9f);
-            efx.Effect(effect, EfxEffectf.ReverbRoomRolloffFactor, 3.1f);
-            efx.Effect(effect, EfxEffectf.ReverbReflectionsGain, 0.723f);
-            efx.Effect(effect, EfxEffectf.ReverbReflectionsDelay, 0.03f);
-            efx.Effect(effect, EfxEffectf.ReverbGain, 0.23f);
+        public ReverbEffect(EffectsExtension efx, int source) : base(efx, source, EfxEffectType.Reverb)
+        {
+            SetEffectProperty(EfxEffectf.ReverbDecayTime, 3.0f);
+            SetEffectProperty(EfxEffectf.ReverbDecayHFRatio, 0.91f);
+            SetEffectProperty(EfxEffectf.ReverbDensity, 0.7f);
+            SetEffectProperty(EfxEffectf.ReverbDiffusion, 0.9f);
+            SetEffectProperty(EfxEffectf.ReverbRoomRolloffFactor, 3.1f);
+            SetEffectProperty(EfxEffectf.ReverbReflectionsGain, 0.723f);
+            SetEffectProperty(EfxEffectf.ReverbReflectionsDelay, 0.03f);
+            SetEffectProperty(EfxEffectf.ReverbGain, 0.23f);
+        }
 
-            efx.AuxiliaryEffectSlot(slot, EfxAuxiliaryi.EffectslotEffect, effect);
-            efx.BindSourceToAuxiliarySlot(source, slot, 0, 0);
+        public void SetPreset(string preset)
+        {
+            float[] result = new float[] { 3.0f, 0.91f, 0.7f, 0.9f, 3.1f, 0.723f, 0.03f, 0.23f };
+            reverbPresets.TryGetValue(preset, out result);
+
+            SetDecayTime(result[0]);
+            SetDecayHFRatio(result[1]);
+            SetDensity(result[2]);
+            SetDiffusion(result[3]);
+            SetRoomRolloffFactor(result[4]);
+            SetReflectionsGain(result[5]);
+            SetReflectionsDelay(result[6]);
+            SetGain(result[7]);
+        }
+
+        public void SetDecayTime(float decayTime)
+        {
+            SetEffectProperty(EfxEffectf.ReverbDecayTime, decayTime);
+        }
+
+        public void SetDecayHFRatio(float decayHFRatio)
+        {
+            SetEffectProperty(EfxEffectf.ReverbDecayHFRatio, decayHFRatio);
+        }
+
+        public void SetDensity(float density)
+        {
+            SetEffectProperty(EfxEffectf.ReverbDensity, density);
+        }
+
+        public void SetDiffusion(float diffusion)
+        {
+            SetEffectProperty(EfxEffectf.ReverbDiffusion, diffusion);
+        }
+
+        public void SetRoomRolloffFactor(float roomRolloffFactor)
+        {
+            SetEffectProperty(EfxEffectf.ReverbRoomRolloffFactor, roomRolloffFactor);
+        }
+
+        public void SetReflectionsGain(float reflectionsGain)
+        {
+            SetEffectProperty(EfxEffectf.ReverbReflectionsGain, reflectionsGain);
+        }
+
+        public void SetReflectionsDelay(float reflectionsDelay)
+        {
+            SetEffectProperty(EfxEffectf.ReverbReflectionsDelay, reflectionsDelay);
+        }
+
+        public void SetGain(float gain)
+        {
+            SetEffectProperty(EfxEffectf.ReverbGain, gain);
         }
     }
 }
