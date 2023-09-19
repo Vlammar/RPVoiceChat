690508b555eec1219cd4a85085da5d1fb119c92d
Rasmus Tanggaard <rasmus@tthost.dk>
2023-09-19 15:16:53 +0200

    Cleanup some source stuff

--- src/Audio/Output/Sources/IAudioSource.cs
+++ src/Audio/Output/Sources/IAudioSource.cs
@@ -3,10 +3,10 @@
     public interface IAudioSource
     {
         public void UpdateSource();
+
         public IAudioCodec GetOrCreateAudioCodec(int frequency, int channels);
 
         public void EnqueueAudio(AudioData audio, long sequenceNumber);
-
         public void DequeueAudio(float _);
 
         public void StartPlaying();
--- src/Audio/Output/Sources/StaticAudioSource.cs
+++ src/Audio/Output/Sources/StaticAudioSource.cs
@@ -1,23 +1,58 @@
-﻿using System;
+﻿using OpenTK.Audio.OpenAL;
+using RPVoiceChat.Utils;
+using System;
 using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Common.Entities;
+using Vintagestory.API.MathTools;
 
 namespace RPVoiceChat.Audio
 {
     public class StaticAudioSource : AudioSource, IAudioSource
     {
+        private Vec3f coords;
+
         /// <summary>
         /// Creates a static audio source.
         /// This audio source does not move relative to the world. (Used for stuff like blocks)
         /// </summary>
         /// <param name="manager"></param>
         /// <param name="capi"></param>
-        public StaticAudioSource(AudioOutputManager manager, ICoreClientAPI capi) : base(manager, capi)
+        public StaticAudioSource(Vec3f vec3f, AudioOutputManager manager, ICoreClientAPI capi) : base(manager, capi)
         {
+            coords = vec3f;
+        }
+
+        public StaticAudioSource(BlockPos blockPos, AudioOutputManager manager, ICoreClientAPI capi) : base(manager, capi)
+        {
+            coords = blockPos.ToVec3f();
+        }
+
+        private Vec3f GetRelativeSourcePosition()
+        {
+            return coords - capi.World.Player.Entity?.SidedPos.AsBlockPos.ToVec3f();
         }
 
         public void UpdateSource()
         {
-            throw new NotImplementedException();
+
+            EntityPos listenerPos = capi.World.Player.Entity?.SidedPos;
+            if (coords == null || listenerPos == null || !outputManager.isReady)
+                return;
+
+
+            float gain = Math.Min(PlayerListener.gain, 1);
+            var sourcePosition = new Vec3f();
+            var velocity = new Vec3f();
+            if (IsLocational)
+            {
+                sourcePosition = GetRelativeSourcePosition();
+            }
+
+            OALW.ClearError();
+            OALW.Source(source, ALSourcef.Gain, gain);
+            OALW.Source(source, ALSource3f.Position, sourcePosition.X, sourcePosition.Y, sourcePosition.Z);
+            OALW.Source(source, ALSourceb.SourceRelative, true);
         }
     }
 }
